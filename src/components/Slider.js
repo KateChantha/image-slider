64028d1c8b94d2b96cf20bc0304cfc0a6fc19c63
Kato <kubkate@gmail.com>
2021-01-18 19:19:39 -0800

    bring Arrow to slide

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import { css } from '@emotion/css'
 import SliderContent from './SliderContent'
 import Slide from './Slide';
+import Arrow from './Arrow'
 
 const Slider = props => {
   const getWidth = () => window.innerWidth
@@ -14,30 +15,32 @@ const Slider = props => {
 
   const { translate, transition } = state
 
-  console.log(props)
+  // width of SliderContent is all images witdth combine
   return (
     <div className={SliderCSS}>
       <SliderContent
         translate={translate}
         transition={transition}
-        width={getWidth()}
+        width={getWidth() * props.slides.length}
       >
         {props.slides.map((slide, index) => (
           <Slide key={slide} content={slide} />
         ))}
         
       </SliderContent>
+      <Arrow />
+      <Arrow />
     </div>
   )
 }
 
+// Overflow: hidden the Slider component and this will place other images content off the screen
 const SliderCSS = css`
   position: relative;
   height: 100vh;
   width: 100vw;
   margin: 0 auto;
   overflow: hidden;
-  ${'' /* background: red; */}
 `
 
 export default Slider
--- src/components/SliderContent.js
+++ src/components/SliderContent.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-// Overfliw the Slider component and place content off the screen
+// Overflow the Slider component and this will place other images content off the screen
 // give SliderContentthe ability to move back and forth horizontally
+// hard code with negative value to slide to the left translateX(-)
 const SliderContent = styled.div`
   transform: translateX(-${props => props.translate}px);
   transition: transform ease-out ${props => props.transition}s;
   height: 100%;
   width: ${props => props.width}px;
   display: flex;
-  /* background: red; */
 `
 
 export default SliderContent;
